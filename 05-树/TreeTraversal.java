7f67e2aa3e4a375c3df8e809efee6d77c4c0260b
Jonty <1195576334@qq.com>
2020-10-12 20:03:58 +0800

    feat: 树的非递归  Morris 版前中后序遍历

--- 05-树/TreeTraversal.java
+++ 05-树/TreeTraversal.java
@@ -1,7 +1,6 @@
+import javax.sound.midi.Soundbank;
 import java.util.Deque;
 import java.util.LinkedList;
-
-
 
 public class TreeTraversal {
 
@@ -49,18 +48,107 @@ public class TreeTraversal {
             System.out.print(stack2.pop().data + " ");
         }
     }
-
     public static void preMorris(BinaryTreeNode root) {
-
+        BinaryTreeNode cur = root;
+        BinaryTreeNode mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null){
+                while (mostRight.right != null && mostRight.right != cur){
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    /**
+                     * 先序遍历
+                     */
+                    System.out.print(cur.data + " ");
+                    cur = cur.left;
+                    continue;
+                }else {
+                    mostRight.right = null;
+                }
+            } else {
+                // 没有左子树的节点只到达一次, 有左子树的节点会访问两次
+                System.out.println(cur.data + " ");
+            }
+            cur = cur.right;
+        }
     }
 
+    public static void inorderMorris(BinaryTreeNode root){
+        BinaryTreeNode cur = root;
+        BinaryTreeNode mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                }else {
+                    mostRight.right = null;
+                }
+            }
+            System.out.println(cur.data + " ");
+            cur = cur.right;
+        }
+    }
 
-    public static void main(String[] args) {
+    public static void postMorris(BinaryTreeNode root) {
+        BinaryTreeNode cur = root;
+        BinaryTreeNode mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                }else {
+                    mostRight.right = null;
+                    printEdge(cur.left);
+                }
+            }
+            cur = cur.right;
+        }
+        printEdge(root);
+    }
+
+    public static void printEdge(BinaryTreeNode node){
+        BinaryTreeNode tail = reverse(node);
+        BinaryTreeNode cur = tail;
+        while (cur != null) {
+            System.out.println(cur.data + " ");
+            cur = cur.right;
+        }
+        reverse(tail);
+    }
+
+    public static BinaryTreeNode reverse(BinaryTreeNode node) {
+        BinaryTreeNode pre = null;
+        BinaryTreeNode next = null;
+        while (node != null) {
+            next = node.right;
+            node.right = pre;
+            pre = node;
+            node = next;
+        }
+        return pre;
+    }
+
+        public static void main(String[] args) {
         BinaryTreeNode root = new BinaryTreeNode(1);
         root.left = new BinaryTreeNode(2);
         root.right = new BinaryTreeNode(3);
         root.left.left = new BinaryTreeNode(4);
         root.left.right = new BinaryTreeNode(5);
-        postorder(root);
+        preMorris(root);
     }
 }
